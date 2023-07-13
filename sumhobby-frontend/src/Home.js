47e599558af622c500eae6653c853ae41dd47134
kimnayu98 <rlaskdbwkd@naver.com>
2023-07-13 15:35:48 +0900

    feat:Home,Lecture 미완

--- sumhobby-frontend/src/Home.js
+++ sumhobby-frontend/src/Home.js
@@ -1,12 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Grid, Typography, Card, CardActionArea, CardContent, CardMedia, Box } from '@mui/material';
+import { call } from './service/ApiService';
 
-const Home = ({classData}) => {
-  
-  // useEffect(() => {
+const Home = () => {
+  const [classData, setClassData] = useState([]);
 
-  // })
+  useEffect(() => {
+    call('/class/top-rated', 'GET', null)
+      .then((response) => {
+        const categorizedClasses = categorizeClasses(response.data);
+        setClassData(categorizedClasses);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const categorizeClasses = (classes) => {
+    const categorized = {};
+
+    // Categorize classes by classCategory
+    for (const classItem of classes) {
+      
+      const category = classItem.classCategory;
+
+      if (categorized[category]) {
+        categorized[category].push(classItem);
+      } else {
+        categorized[category] = [classItem];
+      }
+    }
+    
+    return categorized;
+  };
+
   return (
     <Container component="main" maxWidth="md" style={{ marginTop: '8%' }}>
       <Grid container spacing={2} justifyContent="center">
@@ -16,27 +42,29 @@ const Home = ({classData}) => {
           </Typography>
         </Grid>
       </Grid>
-      <Grid container spacing={2} justifyContent="center">
-        {classData.map((item) => (
-          <Grid item xs={12} sm={6} md={4} key={item.classNum}>
-            <Card>
-              <CardActionArea component={Link} to={`/class/${item.className}`} style={{ textDecoration: 'none' }}>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={item.img}
-                  alt={item.className}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {item.className}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+      {Object.keys(classData).map((category) => (
+        <React.Fragment key={category}>
+          <Typography variant="h6" component="h2" style={{ marginTop: '16px' }}>
+            {category}
+          </Typography>
+          <Grid container spacing={2}>
+            {classData[category].map((item, index) => (
+              <Grid item xs={12} sm={6} md={4} key={item.classNum}>
+                <Card style={{ height: '100%' }}>
+                  <CardActionArea component={Link} to={`/class/${item.className}`} style={{ textDecoration: 'none', height: '100%' }}>
+                    <CardMedia component="img" height="200" image={item.img} alt={item.className} />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {item.className}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </React.Fragment>
+      ))}
       <Box mt={2} textAlign="center">
         <Link to="/class" variant="body2">
           View more
---
+++ sumhobby-frontend/src/LectureDetail.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Typography, Grid, Button, Card, CardActionArea, CardContent } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { call } from "./service/ApiService";
+
+
+const LectureDetail = () => {
+
+//   const [items, setItems] = useState([]);
+//   useEffect(() => {
+//     setItems(item);
+//   }, []);
+
+//   const [lectures, setLectures] = useState([]);
+
+//   useEffect(() => {
+//     call("/lecture", "GET", null)
+//       .then((response) => setLectures(response.data))
+//       .catch((error) => console.error(error));
+//   }, []);
+
+//   const scrollContainerRef = useRef(null);
+
+//   const handleScrollLeft = () => {
+//     if (scrollContainerRef.current) {
+//       scrollContainerRef.current.scrollLeft -= 200;
+//     }
+//   };
+//   const handleScrollRight = () => {
+//     if (scrollContainerRef.current) {
+//       scrollContainerRef.current.scrollLeft += 200; // Adjust the scroll distance as needed
+//     }
+//   };
+
+//   return (
+//     <div className="LectureDetail">
+//       <div className="info-container">
+//         <img src={item.img} className="class-thumbnail" alt="Thumbnail" width={300} height={150} />
+//         <div className="info-row">
+//           <Typography component="span" className="class-name">
+//             제목: {items.className}
+//           </Typography>
+//         </div>
+//         <div className="info-row">
+//           <Typography component="span" className="userTk">
+//             강사: {items.userId}
+//           </Typography>
+//         </div>
+//         <div className="info-row">
+//           <Typography component="span" className="rating">
+//             별점: {items.classRate}
+//           </Typography>
+//         </div>
+//         <div className="info-row">
+//           <Typography component="span" className="class-intro">
+//             소개: {items.classDetail}
+//           </Typography>
+//         </div>
+//         <Link
+//           to={`/showreview?title=${encodeURIComponent(item.className)}&instructorName=${encodeURIComponent(
+//             item.instructorName
+//           )}`}
+//           variant="body2"
+//           className="App-link"
+//         >
+//           View Review
+//         </Link>
+//         <Button onClick={handleClass} variant="body2" className="App-link">
+//           리뷰 작성하기
+//         </Button>
+//       </div>
+
+//       {/* lecture */}
+//       <div className="lecture-container" ref={scrollContainerRef}>
+//         <Typography className="lecture-header" component="h5" align="left" marginTop={3}>
+//           강의 회차
+//         </Typography>
+//         <div className="lecture-scroll-container" ref={scrollContainerRef} >
+//           <Grid container spacing={2} justifyContent="flex-start">
+//             {lectures.slice(0, 6).map((lecture) => (
+//               <Grid item xs={12} sm={4} md={2} key={lecture.lectureNum}>
+//                 <Card>
+//                   <CardActionArea>
+//                     <iframe
+//                       title="YouTube video player"
+//                       width="100%"
+//                       height="200"
+//                       src={lecture.lecUrl}
+//                       frameBorder="0"
+//                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//                       allowFullScreen
+//                     ></iframe>
+//                     <CardContent>
+//                       <Typography gutterBottom variant="subtitle2" component="div">
+//                         {lecture.lecTitle}
+//                       </Typography>
+//                       <Typography variant="caption" color="textSecondary">
+//                         회차: {lecture.lecNum}
+//                       </Typography>
+//                     </CardContent>
+//                   </CardActionArea>
+//                 </Card>
+//               </Grid>
+//             ))}
+//           </Grid>
+//         </div>
+//         {lectures.length > 6 && (
+//           <div className="scroll-buttons">
+//             <button onClick={handleScrollLeft}>&lt;</button>
+//             <button onClick={handleScrollRight}>&gt;</button>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+ };
+
+export default LectureDetail;
