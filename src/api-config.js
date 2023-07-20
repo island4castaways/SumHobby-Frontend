f72f3a46baa75a7ca36907b3b00a72040459ebd7
yeonsu10 <jenix1@naver.com>
2023-07-20 18:34:00 +0900

    commit to deploy

--- src/api-config.js
+++ src/api-config.js
@@ -2,7 +2,7 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
-    backendHost = "http://localhost:1010"
+    backendHost = "http://localhost:5000"
 }
 
 export const API_BASE_URL = `${backendHost}`;
