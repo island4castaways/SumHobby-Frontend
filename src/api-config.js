038a1255034d172b01cf4b378223ef6e5b751053
yeonsu10 <jenix1@naver.com>
2023-07-21 09:58:16 +0900

    feat: aws 설정

--- src/api-config.js
+++ src/api-config.js
@@ -3,6 +3,8 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
     backendHost = "http://localhost:5000"
+}else{
+    backendHost = "http://sumhobby-backend-env.eba-ytrmxek5.ap-northeast-2.elasticbeanstalk.com"
 }
 
 export const API_BASE_URL = `${backendHost}`;
